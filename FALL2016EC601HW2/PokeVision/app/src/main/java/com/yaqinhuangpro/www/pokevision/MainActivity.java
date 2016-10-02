e25668f08c64cf5f80e110e7162b0b0e2278b8c8
yaqinh <yaqinh@bu.edu>
2016-10-01 22:49:36 -0400

    publish success

---
+++ FALL2016EC601HW2/PokeVision/app/src/main/java/com/yaqinhuangpro/www/pokevision/MainActivity.java
@@ -0,0 +1,33 @@
+package com.yaqinhuangpro.www.pokevision;
+
+import android.content.Intent;
+import android.os.Environment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.File;
+
+public class MainActivity extends AppCompatActivity {
+    public final static String EXTRA_MESSAGE = "com.yaqinhuangpro.www.pokevision.MESSAGE";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /** Called when the user clicks the Login button */
+    public void onButtonTap(View v){
+        Toast myToast = Toast.makeText(
+                getApplicationContext(),
+                "log in success",
+                Toast.LENGTH_LONG);
+        myToast.show();
+
+        Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+        //File photo = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES), "picture.jpg");
+        startActivity(intent);
+    }
+}
